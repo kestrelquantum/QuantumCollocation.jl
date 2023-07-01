85fb5b2ded9349fca138320a8c1982eb7d9ba51b
a7b <adityabhardwaj222@gmail.com>
2023-07-01 05:13:12 -0400

    derivatives of integrators working. delta t deriv fixed

--- test/dynamics_tests.jl
+++ test/dynamics_tests.jl
@@ -82,15 +82,14 @@
 
             # test dynamics jacobian
             shape = ((Z.dims.states - 1) * (Z.T - 1), Z.dim * Z.T)
-            #TODO: Z.datavec is not quite right because it includes the timestep so ForwardDiff.jacobian
-            #      gives the wrong result. Need to fix this.
+
             J_dynamics = dense(dynamics.∂F(Z.datavec), dynamics.∂F_structure, shape)
             # display(Z.data)
             # println(Z.dim)
             #display(Z.datavec)
             J_forward_diff = ForwardDiff.jacobian(dynamics.F, Z.datavec)
-            display(J_dynamics)
-            display(J_forward_diff)
+            # display(J_dynamics)
+            # display(J_forward_diff)
             @test all(J_forward_diff .≈ J_dynamics)
             show_diffs(J_forward_diff, J_dynamics)
 
