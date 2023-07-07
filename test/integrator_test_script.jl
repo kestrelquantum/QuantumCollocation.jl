bcf286f79c3efac21c2daf638797c863810ed963
a7b <adityabhardwaj222@gmail.com>
2023-07-07 14:41:23 -0400

    script for benchmarking integration calcuation

---
+++ test/integrator_test_script.jl
@@ -0,0 +1,37 @@
+using Revise
+using QuantumCollocation
+using NamedTrajectories
+using TrajectoryIndexingUtils
+using BenchmarkTools
+using LinearAlgebra
+
+T = 5
+H_drift = kron(GATES[:Z], GATES[:X], GATES[:X], GATES[:X])
+H_drives = [kron(GATES[:X], I(2), I(2), I(2)), kron(GATES[:Y], GATES[:Y], I(2), GATES[:Y])]
+n_drives = length(H_drives)
+
+system = QuantumSystem(H_drift, H_drives)
+
+U_init = I(16)
+U_goal = kron(GATES[:X], GATES[:X], GATES[:X], GATES[:X])
+
+Ũ⃗_init = operator_to_iso_vec(U_init)
+Ũ⃗_goal = operator_to_iso_vec(U_goal)
+
+dt = 0.1
+
+Z = NamedTrajectory(
+    (
+        Ũ⃗ = unitary_geodesic(U_goal, T),
+        a = randn(n_drives, T),
+        da = randn(n_drives, T),
+        Δt = fill(dt, 1, T),
+    ),
+    controls=(:da,),
+    timestep=:Δt,
+    goal=(Ũ⃗ = Ũ⃗_goal,)
+)
+
+P = UnitaryPadeIntegrator(system, :Ũ⃗, :a)
+
+@btime P(Z.datavec[slice(2, Z.dim)], Z.datavec[slice(3, Z.dim)], Z)
