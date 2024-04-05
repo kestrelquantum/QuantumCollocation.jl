46d62842503be391f386d115849ff580dd57b3a0
andy <agoldschmidt11@gmail.com>
2024-04-05 10:39:12 -0500

    update graph arg & system accepts params

--- src/problem_templates/unitary_direct_sum_problem.jl
+++ src/problem_templates/unitary_direct_sum_problem.jl
@@ -2,38 +2,35 @@
     UnitaryDirectSumProblem(probs, final_fidelity; kwargs...)
 
 Construct a `QuantumControlProblem` as a direct sum of unitary gate problems. The 
-purpose is to find solutions that are as close as possible in the sense of the
-trajectories of the unitaries that implement each gate. In particular, this is 
-useful for finding interpolatable control solutions.
+purpose is to find solutions that are as close as possible with respect to one of 
+their components. In particular, this is useful for finding interpolatable control solutions.
 
-    TODO: Direct sum problems are more general than this. The main innovation
-    is to use objectives to couple otherwise uncoupled problems.
+A graph of edges (specified by problem labels) will enforce a `PairwiseQuadraticRegularizer` between
+the component trajectories of the problem in `probs` corresponding to the names of the edge in `edges`
+with corresponding edge weight `Q`.
 
-A graph of edges will enforce a `UnitaryPairwiseQuadraticRegularizer` between
-the unitary trajectories of the problem in `probs` corresponding to the index of
-the edge in `edges` with corresponding edge weight `Q`.
-
-The default behavior is to use a 1D chain for the graph, i.e., enforce a 
-`UnitaryPairwiseQuadraticRegularizer` between each neighbor of the provided `probs`.
+The default behavior is to use a 1D chain for the graph, i.e., enforce a `PairwiseQuadraticRegularizer`
+between each neighbor of the provided `probs`.
 
 # Arguments
-
 - `probs::AbstractVector{<:QuantumControlProblem}`: the problems to combine
 - `final_fidelity::Real`: the fidelity to enforce between the component final unitaries and the component goal unitaries
 
 # Keyword Arguments
-
-- `prob_labels::Union{Nothing, AbstractVector{<:String}}=[String(i) for i ∈ 1:length(probs)]`: the labels for the problems
-- `graph::Union{Nothing, AbstractVector{<:AbstractVector{<:String}}}=nothing`: the graph of edges to enforce
+- `prob_labels::AbstractVector{<:String}}`: the labels for the problems
+-  graph::Union{Nothing, AbstractVector{<:Tuple{String, String}}, AbstractVector{<:Tuple{Symbol, Symbol}}}`: the graph of edges to enforce
 - `Q::Union{Float64, Vector{Float64}}=100.0`: the weights on the pairwise regularizers
-- `R::Float64=1e-2`: the shared weight on all control terms
+- `Q_symb::Symbol=:Ũ⃗`: the symbol to use for the regularizer
+- `R::Float64=1e-2`: the shared weight on all control terms (:a, :da, :dda is assumed)
 - `R_a::Union{Float64, Vector{Float64}}=R`: the weight on the regularization term for the control pulses
 - `R_da::Union{Float64, Vector{Float64}}=R`: the weight on the regularization term for the control pulse derivatives
 - `R_dda::Union{Float64, Vector{Float64}}=R`: the weight on the regularization term for the control pulse second derivatives
-- `subspace::Union{AbstractVector{<:Integer}, Nothing}=nothing`: the subspace to use for the fidelity
-- `pade_order=4`: the order of the Pade approximation to use for the unitary integrator
-- `autodiff=pade_order!=4`: whether or not to use automatic differentiation for the unitary integrator
+- `subspace::Union{AbstractVector{<:Integer}, Nothing}=nothing`: the subspace to use for the fidelity of each problem
+- `max_iter::Int=1000`: the maximum number of iterations for the Ipopt solver
+- `linear_solver::String="mumps"`: the linear solver to use in Ipopt
 - `hessian_approximation=true`: whether or not to use L-BFGS hessian approximation in Ipopt
+- `jacobian_structure=true`: whether or not to use the jacobian structure in Ipopt
+- `blas_multithreading=true`: whether or not to use multithreading in BLAS
 - `ipopt_options::Options=Options()`: the options for the Ipopt solver
 
 """
@@ -41,17 +38,17 @@ function UnitaryDirectSumProblem(
     probs::AbstractVector{<:QuantumControlProblem},
     final_fidelity::Real;
     prob_labels::AbstractVector{<:String}=[string(i) for i ∈ 1:length(probs)],
-    graph::Union{Nothing, AbstractVector{<:Tuple{Symbol, Symbol}}}=nothing,
+    graph::Union{Nothing, AbstractVector{<:Tuple{String, String}}, AbstractVector{<:Tuple{Symbol, Symbol}}}=nothing,
     Q::Union{Float64, Vector{Float64}}=100.0,
     Q_symb::Symbol=:Ũ⃗,
     R::Float64=1e-2,
     R_a::Union{Float64, Vector{Float64}}=R,
     R_da::Union{Float64, Vector{Float64}}=R,
     R_dda::Union{Float64, Vector{Float64}}=R,
+    subspace::Union{AbstractVector{<:Integer}, Nothing}=nothing,
     max_iter::Int=1000,
     linear_solver::String="mumps",
     verbose::Bool=false,
-    subspace::Union{AbstractVector{<:Integer}, Nothing}=nothing,
     hessian_approximation=true,
     jacobian_structure=true,
     blas_multithreading=true,
@@ -81,6 +78,18 @@ function UnitaryDirectSumProblem(
             (apply_postfix(Q_symb, i), apply_postfix(Q_symb, j))
             for (i, j) ∈ zip(prob_labels[1:N-1], prob_labels[2:N])
         ]
+    else
+        # Check that String edge labels are in prob_labels, and make Symbols
+        if eltype(eltype(graph)) == String
+            graph_symb = Tuple{Symbol, Symbol}[]
+            for edge in graph
+                if !(edge[1] in prob_labels && edge[2] in prob_labels)
+                    throw(ArgumentError("Edge labels must be in prob_labels"))
+                end
+                push!(graph_symb, (apply_postfix(Q_symb, edge[1]), apply_postfix(Q_symb, edge[2])))
+            end
+            graph = graph_symb
+        end
     end
 
     # Build the direct sum system
@@ -96,9 +105,11 @@ function UnitaryDirectSumProblem(
         [apply_postfix(p.integrators, ℓ) for (p, ℓ) ∈ zip(probs, prob_labels)]...
     )
 
-    # TODO: How does this get used?
-    # TODO: If parameters, need to apply postfix to merge safely
-    system = reduce(direct_sum, [p.system for p ∈ probs])
+    # direct sum (used for problem saving, only)
+    system = reduce(
+        direct_sum, 
+        [apply_postfix(p.system, ℓ) for (p, ℓ) ∈ zip(probs, prob_labels)]
+    )
 
     # Rebuild trajectory constraints
     build_trajectory_constraints = true
@@ -120,7 +131,6 @@ function UnitaryDirectSumProblem(
     J = PairwiseQuadraticRegularizer(traj, Q, graph)
 
     for (p, ℓ) ∈ zip(probs, prob_labels)
-        # TODO: not generic
         J += QuadraticRegularizer(apply_postfix(:a, ℓ), traj, R_a)
         J += QuadraticRegularizer(apply_postfix(:da, ℓ), traj, R_da)
         J += QuadraticRegularizer(apply_postfix(:dda, ℓ), traj, R_dda)
@@ -168,6 +178,7 @@ end
 apply_postfix(symb::Symbol, postfix::String) = Symbol(string(symb, postfix))
 apply_postfix(symb::Tuple, postfix::String) = Tuple(apply_postfix(s, postfix) for s ∈ symb)
 apply_postfix(symb::AbstractVector, postfix::String) = [apply_postfix(s, postfix) for s ∈ symb]
+apply_postfix(d::Dict{Symbol, Any}, postfix::String) = typeof(d)(apply_postfix(k, postfix) => v for (k, v) ∈ d)
 
 function apply_postfix(nt::NamedTuple, postfix::String)
     symbs = Tuple(apply_postfix(k, postfix) for k ∈ keys(nt))
@@ -238,6 +249,14 @@ end
 
 apply_postfix(integrators::AbstractVector{<:AbstractIntegrator}, postfix::String) =
     [apply_postfix(integrator, postfix) for integrator ∈ integrators]
+
+function apply_postfix(sys::QuantumSystem, postfix::String)
+    return QuantumSystem(
+        sys.H_drift,
+        sys.H_drives,
+        params=apply_postfix(sys.params, postfix)
+    )
+end
 
 function merge_outer(nt1::NamedTuple, nt2::NamedTuple)
     common_keys = intersect(keys(nt1), keys(nt2))
@@ -338,4 +357,100 @@ end
 
     @test issetequal(new_traj.state_names, vcat(pf_traj1.state_names..., pf_traj2.state_names...))
     @test issetequal(new_traj.control_names, vcat(pf_traj1.control_names..., pf_traj2.control_names...))
+end
+
+@testitem "Merge systems" begin
+    using NamedTrajectories
+    include("../../test/test_utils.jl")
+
+    H_drift = 0.01 * GATES[:Z]
+    H_drives = [GATES[:X], GATES[:Y]]
+    T = 50
+    sys = QuantumSystem(H_drift, H_drives, params=Dict(:T=>T))
+        
+    # apply postfix and sum
+    sys2 = direct_sum(
+        ProblemTemplates.apply_postfix(sys, "_1"),
+         ProblemTemplates.apply_postfix(sys, "_2")
+    )
+
+    @test length(sys2.H_drives) == 4
+    @test sys2.params[:T_1] == T
+    @test sys2.params[:T_2] == T
+
+    # add another system
+    sys = QuantumSystem(H_drift, H_drives, params=Dict(:T=>T, :S=>2T))
+    sys3 = direct_sum(sys2, ProblemTemplates.apply_postfix(sys, "_3"))
+    @test length(sys3.H_drives) == 6
+    @test sys3.params[:T_3] == T
+    @test sys3.params[:S_3] == 2T
+end
+
+@testitem "Construct problem" begin
+    sys = QuantumSystem(0.01 * GATES[:Z], [GATES[:X], GATES[:Y]])
+    U_goal1 = GATES[:X]
+    U_ε = haar_identity(2, 0.33)
+    U_goal2 = U_ε'U_goal1*U_ε
+    T = 50
+    Δt = 0.2
+    ops = Options(recalc_y="yes", recalc_y_feas_tol=0.01, print_level=1)
+    prob1 = UnitarySmoothPulseProblem(sys, U_goal1, T, Δt, free_time=false, ipopt_options=ops)
+    prob2 = UnitarySmoothPulseProblem(sys, U_goal2, T, Δt, free_time=false, ipopt_options=ops)
+
+    # Test default
+    direct_sum_prob1 = UnitaryDirectSumProblem([prob1, prob2], 0.99, ipopt_options=ops)
+    state_names = vcat(
+        ProblemTemplates.apply_postfix(prob1.trajectory.state_names, "1")...,
+        ProblemTemplates.apply_postfix(prob2.trajectory.state_names, "2")...
+    )
+    control_names = vcat(
+        ProblemTemplates.apply_postfix(prob1.trajectory.control_names, "1")...,
+        ProblemTemplates.apply_postfix(prob2.trajectory.control_names, "2")...
+    )
+    @test issetequal(direct_sum_prob1.trajectory.state_names, state_names)
+    @test issetequal(direct_sum_prob1.trajectory.control_names, control_names)
+
+    # Test label graph
+    direct_sum_prob2 = UnitaryDirectSumProblem(
+        [prob1, prob2], 
+        0.99, 
+        prob_labels=["a", "b"],
+        graph=[("a", "b")],
+        ipopt_options=ops)
+    state_names_ab = vcat(
+        ProblemTemplates.apply_postfix(prob1.trajectory.state_names, "a")...,
+        ProblemTemplates.apply_postfix(prob2.trajectory.state_names, "b")...
+    )
+    control_names_ab = vcat(
+        ProblemTemplates.apply_postfix(prob1.trajectory.control_names, "a")...,
+        ProblemTemplates.apply_postfix(prob2.trajectory.control_names, "b")...
+    )
+    @test issetequal(direct_sum_prob2.trajectory.state_names, state_names_ab)
+    @test issetequal(direct_sum_prob2.trajectory.control_names, control_names_ab)
+
+    # Test bad graph
+    @test_throws ArgumentError UnitaryDirectSumProblem(
+        [prob1, prob2], 
+        0.99, 
+        prob_labels=["a", "b"],
+        graph=[("x", "b")],
+        ipopt_options=ops
+    )
+
+    # Test symbol graph
+    direct_sum_prob3 = UnitaryDirectSumProblem(
+        [prob1, prob2], 
+        0.99, 
+        graph=[(:a1, :a2)],
+        ipopt_options=ops
+    )
+    @test issetequal(direct_sum_prob3.trajectory.state_names, state_names)
+    @test issetequal(direct_sum_prob3.trajectory.control_names, control_names)
+
+    # Test triple
+    direct_sum_prob4 = UnitaryDirectSumProblem([prob1, prob2, prob1], 0.99, ipopt_options=ops)
+    state_names_triple = vcat(state_names..., ProblemTemplates.apply_postfix(prob1.trajectory.state_names, "3")...)
+    control_names_triple = vcat(control_names..., ProblemTemplates.apply_postfix(prob1.trajectory.control_names, "3")...)
+    @test issetequal(direct_sum_prob4.trajectory.state_names, state_names_triple)
+    @test issetequal(direct_sum_prob4.trajectory.control_names, control_names_triple)
 end
