7305f5af5395b4ee7e20b861d9b7dc8f73814998
Mahnoor Fatima <mahnoorf04@gmail.com>
2024-06-01 03:43:19 +0500

    Add optics.jl to quantum_system_templates

--- src/quantum_system_templates/_quantum_system_templates.jl
+++ src/quantum_system_templates/_quantum_system_templates.jl
@@ -4,13 +4,16 @@ export TransmonSystem
 export TransmonDipoleCoupling
 export MultiTransmonSystem
 export RydbergChainSystem
+export OpticSystem
 
 using ..QuantumUtils
 using ..QuantumSystems
 
 using LinearAlgebra
+using QuantumOptics
 
 include("transmons.jl")
 include("rydberg.jl")
+include("optics.jl")
 
 end
