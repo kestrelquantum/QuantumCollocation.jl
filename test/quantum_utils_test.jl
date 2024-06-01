d9998bf2c3de0f8c4b80f96f1b54be7a49985619
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-01 12:24:28 +0500

    Adding test for isomorphism utilities

--- test/quantum_utils_test.jl
+++ test/quantum_utils_test.jl
@@ -33,7 +33,8 @@ end
  									 0   0    0	im]
     #Associativity: (X ⊗ Y) ⊗ Z  == X ⊗ (Y  ⊗ Z)
     @test(GATES[:X] ⊗ GATES[:Y]) ⊗ GATES[:Z] == GATES[:X] ⊗ (GATES[:Y] ⊗ GATES[:Z])
-    #Distributivity: A ⊗( B + C ) = A ⊗ B + A ⊗ C    @test GATES[:X] ⊗ (GATES[:Y] + GATES[:Z]) == GATES[:X] ⊗ GATES[:Y] + GATES[:X] ⊗ GATES[:Z]
+    #Distributivity: A ⊗( B + C ) = A ⊗ B + A ⊗ C    
+    @test GATES[:X] ⊗ (GATES[:Y] + GATES[:Z]) == GATES[:X] ⊗ GATES[:Y] + GATES[:X] ⊗ GATES[:Z]
 end 
 
 @testitem "Test apply function with Pauli gates" begin
@@ -77,4 +78,32 @@ end
 
     #Lift 2x2 identity matrix U to the first qubit in a 4-qubit system
     @test size(lift(U1, 1, 3)) == (16, 16)
+end
+
+# Define test cases for isomorphism utilities
+@testitem "Test isomorphism utilities" begin
+    using QuantumUtils
+    # Test vector for conversion
+    iso_vec = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]
+    
+    # Test vec⁻¹ function: Converts a vector into a square matrix.
+    @test vec⁻¹([1.0, 2.0, 3.0, 4.0]) == [1.0 3.0; 2.0 4.0]
+
+    # Test ket_to_iso function: Converts a ket vector into a complex vector with real and imaginary parts.
+    @test ket_to_iso([1.0, 2.0]) == [1.0, 2.0, 0.0, 0.0]
+
+    # Test iso_to_ket function: Converts a complex vector with real and imaginary parts into a ket vector.
+    @test iso_to_ket([1.0, 2.0, 0.0, 0.0]) == [1.0, 2.0]
+
+    # Test iso_vec_to_operator function: Converts a complex vector into a complex matrix representing an operator.
+    @test iso_vec_to_operator(iso_vec) == [1.0 0.0; 0.0 1.0]
+
+    # Test iso_vec_to_iso_operator function: Converts a complex vector into a real matrix representing an isomorphism operator.
+    @test iso_vec_to_iso_operator(iso_vec) == [1.0 0.0 -0.0 -0.0; 0.0 1.0 -0.0 -0.0; 0.0 0.0 1.0 0.0; 0.0 0.0 0.0 1.0]
+
+    # Test operator_to_iso_vec function: Converts a complex matrix representing an operator into a complex vector.
+    @test operator_to_iso_vec(Complex[1.0 0.0; 0.0 1.0]) == iso_vec
+    
+    # Test iso_operator_to_iso_vec function: Converts a real matrix representing an isomorphism operator into a complex vector.
+    @test iso_operator_to_iso_vec(iso_vec_to_iso_operator(iso_vec)) == iso_vec
 end
