49a52ba86b896abceb7321d494d07130e4dc9d01
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-01 15:06:13 +0500

    Setting up for tests

--- test/quantum_utils_test.jl
+++ test/quantum_utils_test.jl
@@ -3,19 +3,20 @@ Tests: QuantumUtils submodule
 """
 
 @testitem "GATES" begin
+    using LinearAlgebra
     @test get_gate(:X) * get_gate(:X) ==  get_gate(:I)
     @test get_gate(:Y) * get_gate(:Y) ==  get_gate(:I)
-    @test -get_gate(:Z) * get_gate(:Z) ==  get_gate(:I)
-    @test -get_gate(:X) * get_gate(:Y) == get_gate(:Z)
     # There is some imprecision in floating-point arithmetic, 
     # which is why we use a tolerance to check for approximate equality.
 
     #H*X*H† = Z
     @test isapprox(GATES[:H]*GATES[:X]*GATES[:H]', GATES[:Z], atol=1e-2) == true
     @test isapprox(get_gate(:H)*get_gate(:X)*get_gate(:H)', get_gate(:Z), atol=1e-2) == true
+
     #H*Z*H† = X
     @test isapprox(GATES[:H]*GATES[:Z]*GATES[:H]', GATES[:X], atol=1e-2) == true
     @test isapprox(get_gate(:H)*get_gate(:Z)*get_gate(:H)', get_gate(:X), atol=1e-2) == true
+
     #H*Y*H† = -Y
     @test isapprox(GATES[:H]*GATES[:Y]*GATES[:H]', -GATES[:Y], atol=1e-2) == true
     @test isapprox(get_gate(:H)*get_gate(:Y)*get_gate(:H)', -get_gate(:Y), atol=1e-2) == true
@@ -29,34 +30,31 @@ Tests: QuantumUtils submodule
     @test get_gate(:CX)*get_gate(:CX) == I(4)
 
     @test Int.(round.(real.(GATES[:H]*GATES[:H]))) == GATES[:I]
+    # X² = I
     @test GATES[:X] ^ 2 == GATES[:I]
+    @test isapprox(GATES[:X] ^ 2, GATES[:I], atol=1e-2) == true
+    # Y² = I
     @test GATES[:Y] ^ 2 == GATES[:I]
-    @test GATES[:Z] ^ 2 == -GATES[:I]
+    @test isapprox(GATES[:Y] ^ 2, GATES[:I], atol=1e-2) == true
+    # Z² = I
+    @test isapprox(GATES[:Z] ^ 2, GATES[:I], atol=1e-2) == true
+
     @test Int.(round.(real.(GATES[:X]*GATES[:Y] + GATES[:Y]*GATES[:X]))) == zeros(2, 2)
     @test Int.(round.(real.(GATES[:Y]*GATES[:Z] + GATES[:Z]*GATES[:Y]))) == zeros(2, 2)
     @test Int.(round.(real.(GATES[:Z]*GATES[:X] + GATES[:X]*GATES[:Z]))) == zeros(2, 2)
 end
 
 @testitem "⊗" begin
-    @test Int.(round.(real.(GATES[:X] ⊗ GATES[:X]))) == [0  0  0  1;
-											 0  0  1  0;
-											 0  1  0  0;
- 											 1  0  0  0]
-    @test Int.(round.(real.(GATES[:X] ⊗ GATES[:I]))) == [0  0  1  0;
-											 0  0  0  1;
-											 1  0  0  0;
- 										 	 0  1  0  0]
-    @test complex.(GATES[:Z] ⊗ GATES[:I]) == [-im	0	0 	0;
-								      0	-im	0	0;
-									 0   0	im   0;
- 									 0   0    0	im]
-    #Associativity: (X ⊗ Y) ⊗ Z  == X ⊗ (Y  ⊗ Z)
-    @test(GATES[:X] ⊗ GATES[:Y]) ⊗ GATES[:Z] == GATES[:X] ⊗ (GATES[:Y] ⊗ GATES[:Z])
-    #Distributivity: A ⊗( B + C ) = A ⊗ B + A ⊗ C    
+    using LinearAlgebra
+    @test Int.(real.(GATES[:I] ⊗ GATES[:I])) == I(4)
+    #Associativity
+    @test (GATES[:X] ⊗ GATES[:Y]) ⊗ GATES[:Z] == GATES[:X] ⊗ (GATES[:Y] ⊗ GATES[:Z])
+    #Distributivity    
     @test GATES[:X] ⊗ (GATES[:Y] + GATES[:Z]) == GATES[:X] ⊗ GATES[:Y] + GATES[:X] ⊗ GATES[:Z]
 end 
 
 @testitem "Test apply function with Pauli gates" begin
+    using LinearAlgebra
     # Define the initial state
     ψ₀ = [1.0 + 0.0im, 0.0 + 0.0im]  # Example initial state for a single qubit
     ψ₁ = [0.0 + 0.0im, 1.0 + 0.0im]
@@ -71,7 +69,8 @@ end
     @test apply(:Y, ψ₀) == GATES[:Y] * ψ₀  # Apply the Y gate to the state |0⟩
     @test apply(:Y, ψ₁) == GATES[:Y] * ψ₁  # Apply the Y gate to the state |1⟩
    
-    @test apply(:H, apply(:H, [1,  0])) == [1, 0] == true
+    # H * H = I 
+    @test apply(:H, apply(:H, [1,  0])) == [1, 0]
     @test isapprox(apply(:H, apply(:H, apply(:H, [1,  0]))), [0.7071, 0.7071], atol=1e-1) == true
     
     # CNOT |00⟩ = |00⟩
@@ -87,6 +86,7 @@ end
 
 # Define a test set for qubit_system_state function
 @testitem "Test qubit_system_state function" begin
+    using LinearAlgebra
     @test qubit_system_state("0") == [1, 0]
     @test qubit_system_state("1") == [0, 1]
     @test qubit_system_state("00") == [1, 0, 0, 0]
@@ -97,6 +97,7 @@ end
 
 # Define a test set for lift function
 @testitem "Test lift function" begin
+    using LinearAlgebra
     #Lift 2x2 identity matrix U to the first qubit in a 2-qubit system
     U1 = [1 0; 0 1]  # Identity matrix
     @test size(lift(U1, 1, 2)) == (4, 4) 
@@ -105,11 +106,12 @@ end
     @test size(lift(U1, 2, 2)) == (4, 4) 
 
     #Lift 2x2 identity matrix U to the first qubit in a 4-qubit system
-    @test size(lift(U1, 1, 3)) == (16, 16)
+    @test size(lift(U1, 1, 3)) == (8, 8)
 end
 
 # Define test cases for isomorphism utilities
 @testitem "Test isomorphism utilities" begin
+    using LinearAlgebra
     # vector for conversion
     iso_vec = [1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0]
     
@@ -138,6 +140,7 @@ end
 
 # Define test cases for quantum harmonic oscillator operators
 @testitem "quantum harmonic oscillator operators" begin
+    using LinearAlgebra
     # Define a tolerance for floating-point comparisons
     const tol = 1e-10
     # Test with 2 levels
@@ -164,8 +167,8 @@ end
     @test annihilate(4) == create(4)'
    
     # `number = a'a` computes the number operator for a system with `levels` levels.
-    @test number(3) == create(3)* annihilate(3) == true
-    @test number(4) == create(4)* annihilate(4) == true 
+    @test number(3) == create(3)* annihilate(3) 
+    @test number(4) == create(4)* annihilate(4)
     
     # `Quad = `n(n - I)` for a quantum system with `levels` energy levels, where `n` is the number operator and `I` is the identity operator.
     @test quad(3) == number(3) * (number(3) - I(3))
