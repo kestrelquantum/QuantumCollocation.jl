88c5ef13e18b92e43773d70b608584ea11a17dfd
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2024-06-05 13:25:29 -0400

    formatting

--- src/quantum_system_templates/rydberg.jl
+++ src/quantum_system_templates/rydberg.jl
@@ -1,3 +1,34 @@
+
+function generate_pattern(N::Int, i::Int)
+    # Create an array filled with 'I'
+    qubits = fill('I', N)
+    # Insert 'n' at position i and i+1, ensuring it doesn't exceed the array bounds
+    if i <= N && i+1 <= N
+        qubits[i] = 'n'
+        qubits[i+1] = 'n'
+    end
+    return join(qubits)
+end
+function generate_pattern_with_gap(N::Int, i::Int, gap::Int)
+    # Create an array filled with 'I'
+    qubits = fill('I', N)
+    # Insert 'n' at position i and i+gap+1, ensuring it doesn't exceed the array bounds
+    if i <= N && i+gap+1 <= N
+        qubits[i] = 'n'
+        qubits[i+gap+1] = 'n'
+    end
+    return join(qubits)
+end
+
+"""
+Embed a character into a string at a specific position.
+"""
+function lift(x::Char,i::Int, N::Int)
+    qubits = fill('I', N)
+    qubits[i] = x
+    return join(qubits)
+end
+
 @doc raw"""
     RydbergChainSystem(;
         C::Float64=862690*2π,
@@ -20,36 +51,6 @@ H = \sum_i 0.5*\Omega_i(t)\cos(\phi_i(t)) \sigma_i^x - 0.5*\Omega_i(t)\sin(\phi_
 - `cutoff_order`: Interaction range cutoff, 1 is nearest neighbor, 2 is next nearest neighbor.
 - `local_detune`: If true, include one local detuning pattern.
 """
-function generate_pattern(N::Int, i::Int)
-    # Create an array filled with 'I'
-    qubits = fill('I', N)
-    # Insert 'n' at position i and i+1, ensuring it doesn't exceed the array bounds
-    if i <= N && i+1 <= N
-        qubits[i] = 'n'
-        qubits[i+1] = 'n'
-    end
-    return join(qubits)
-end
-function generate_pattern_with_gap(N::Int, i::Int, gap::Int)
-    # Create an array filled with 'I'
-    qubits = fill('I', N)
-    # Insert 'n' at position i and i+gap+1, ensuring it doesn't exceed the array bounds
-    if i <= N && i+gap+1 <= N
-        qubits[i] = 'n'
-        qubits[i+gap+1] = 'n'
-    end
-    return join(qubits)
-end
-"""
-Embed a character into a string at a specific position.
-"""
-function lift(x::Char,i::Int, N::Int)
-    qubits = fill('I', N)
-    qubits[i] = x
-    return join(qubits)
-end
-
-
 function RydbergChainSystem(;
     N::Int=3, # number of atoms
     C::Float64=862690*2π,
@@ -64,7 +65,10 @@ function RydbergChainSystem(;
         H_drift = zeros(ComplexF64, 2^N, 2^N)
         for gap in 0:N-2
             for i in 1:N-gap-1
-                H_drift += C*kron_from_dict(generate_pattern_with_gap(N,i,gap),PAULIS)/(((gap+1)*distance)^6)
+                H_drift += C * kron_from_dict(
+                    generate_pattern_with_gap(N, i, gap),
+                    PAULIS
+                ) / ((gap + 1) * distance)^6
             end
         end
     else
@@ -77,18 +81,23 @@ function RydbergChainSystem(;
             error("Higher cutoff order not supported")
         end
     end
+
     H_drives = Matrix{ComplexF64}[]
+
     # Add global X drive
     Hx = sum([0.5*kron_from_dict(lift('X',i,N), PAULIS) for i in 1:N])
     push!(H_drives, Hx)
+
     if !ignore_Y_drive
         # Add global Y drive
         Hy = sum([0.5*kron_from_dict(lift('Y',i,N), PAULIS) for i in 1:N])
         push!(H_drives, Hy)
     end
+
     # Add global detuning
     H_detune = -sum([kron_from_dict(lift('n',i,N), PAULIS) for i in 1:N])
     push!(H_drives, H_detune)
+
     params = Dict{Symbol, Any}(
         :N => N,
         :C => C,
@@ -97,6 +106,7 @@ function RydbergChainSystem(;
         :local_detune => local_detune,
         :all2all => all2all,
     )
+
     return QuantumSystem(
         H_drift,
         H_drives;
