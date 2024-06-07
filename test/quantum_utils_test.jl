875d5ee61299b9316c4b8b5dc9f288c7b43e7297
Fe-r-oz <ferozahmad7cf@gmail.com>
2024-06-07 22:17:03 +0500

    polishing up

--- test/quantum_utils_test.jl
+++ test/quantum_utils_test.jl
@@ -6,30 +6,23 @@ Tests: QuantumUtils submodule
     using LinearAlgebra
     @test get_gate(:X) * get_gate(:X) ==  get_gate(:I)
     @test get_gate(:Y) * get_gate(:Y) ==  get_gate(:I)
-
-    # Cayley Pauli Table
-    @test im*get_gate(:Y)*get_gate(:X) == get_gate(:Z)  # Z = iYX 
+    @test im*get_gate(:Y)*get_gate(:X) == get_gate(:Z)  # Z = iYX
     @test -im*get_gate(:X)*get_gate(:Y) == get_gate(:Z) # Z = -iXY 
     @test -im*get_gate(:Z)*get_gate(:X) == get_gate(:Y) # Y = -iZX
     @test im*get_gate(:X)*get_gate(:Z) == get_gate(:Y)  # Y = iXZ
     @test im*get_gate(:Z)*get_gate(:Y) == get_gate(:X)  # X = iZY
     @test -im*get_gate(:Y)*get_gate(:Z) == get_gate(:X) # X = -iYZ
 
-    # H*X*H† = Z, H*Z*H† = X, H*Y*H† = -Y
-    @test isapprox(get_gate(:H)*get_gate(:X)*get_gate(:H)', get_gate(:Z), atol=1e-2) == true
-    @test isapprox(get_gate(:H)*get_gate(:Z)*get_gate(:H)', get_gate(:X), atol=1e-2) == true
-    @test isapprox(get_gate(:H)*get_gate(:Y)*get_gate(:H)', -get_gate(:Y), atol=1e-2) == true
+    @test isapprox(get_gate(:H)*get_gate(:X)*get_gate(:H)', get_gate(:Z), atol=1e-2) == true # H*X*H† = Z
+    @test isapprox(get_gate(:H)*get_gate(:Z)*get_gate(:H)', get_gate(:X), atol=1e-2) == true # H*Z*H† = X
+    @test isapprox(get_gate(:H)*get_gate(:Y)*get_gate(:H)', -get_gate(:Y), atol=1e-2) == true # H*Y*H† = -Y
     
-    #H² = I, CNOT² = I, CZ² = I, X² = I, Y² = I, Z² = I
-    @test isapprox(get_gate(:H)*get_gate(:H), get_gate(:I), atol=1e-2) == true
-    @test Int.(round.(real.(GATES[:H]*GATES[:H]))) == GATES[:I]
-    @test GATES[:CX]*GATES[:CX] == I(4)
-    @test GATES[:CZ]*GATES[:CZ] == I(4)
-    @test get_gate(:CX)*get_gate(:CX) == I(4)
-    @test get_gate(:CZ)*get_gate(:CZ) == I(4)
-    @test GATES[:X] ^ 2 == GATES[:I]
-    @test GATES[:Y] ^ 2 == GATES[:I]
-    @test isapprox(GATES[:Z] ^ 2, GATES[:I], atol=1e-2) == true
+    @test isapprox(get_gate(:H)*get_gate(:H), get_gate(:I), atol=1e-2) == true # H² = I
+    @test get_gate(:CX)*get_gate(:CX) == I(4) # CNOT² = I
+    @test get_gate(:CZ)*get_gate(:CZ) == I(4) # CZ² = I
+    @test GATES[:X] ^ 2 == GATES[:I] # X² = I
+    @test GATES[:Y] ^ 2 == GATES[:I] # Y² = I
+    @test isapprox(GATES[:Z] ^ 2, GATES[:I], atol=1e-2) == true # Z² = I
     @test Int.(round.(real.(GATES[:X]*GATES[:Y] + GATES[:Y]*GATES[:X]))) == zeros(2, 2)
     @test Int.(round.(real.(GATES[:Y]*GATES[:Z] + GATES[:Z]*GATES[:Y]))) == zeros(2, 2)
     @test Int.(round.(real.(GATES[:Z]*GATES[:X] + GATES[:X]*GATES[:Z]))) == zeros(2, 2)
@@ -50,9 +43,7 @@ end
     @test apply(:X, ψ₁) == [1.0 + 0.0im, 0.0 + 0.0im]
     @test apply(:Y, ψ₀) == GATES[:Y] * ψ₀  
     @test apply(:Y, ψ₁) == GATES[:Y] * ψ₁
-   
-    # H * H = I 
-    @test apply(:H, apply(:H, [1,  0])) == [1, 0]
+    @test apply(:H, apply(:H, [1,  0])) == [1, 0]  # H * H = I 
     @test isapprox(apply(:H, apply(:H, apply(:H, [1,  0]))), [0.7071, 0.7071], atol=1e-1) == true
     @test apply(:CX, [1, 0, 0, 0]) == [1, 0, 0, 0]  # CNOT |00⟩ = |00⟩
     @test apply(:CX, [0, 1, 0, 0]) == [0, 1, 0, 0]  # CNOT |01⟩ = |01⟩
