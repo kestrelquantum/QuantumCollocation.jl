1948bcb6a27f648793d870b1f9243d65c7c397f4
maxwell04 <mahnoorf04@gmail.com>
2024-06-12 17:54:51 +0500

    Fix a comment

--- src/quantum_system_templates/quantum_optics.jl
+++ src/quantum_system_templates/quantum_optics.jl
@@ -5,7 +5,7 @@ function QuantumOpticsSystem(
     Op_drift::QuantumObject{SparseMatrixCSC{ComplexF64, Int64}, OperatorQuantumObject},
     Op_drives::Vector{QuantumObject{SparseMatrixCSC{ComplexF64, Int64}, OperatorQuantumObject}};
 )
-    # Check for Hermitian matriced
+    # Check for Hermitian matrices
     @assert QuantumToolbox.ishermitian(Op_drift) "Non-Hermitian Hamiltonian provided."
     @assert all([
         QuantumToolbox.ishermitian(Op_drive) for Op_drive in Op_drives
