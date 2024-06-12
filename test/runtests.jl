4f23a3dfbf7e8af40c5e6ee064ff5955b9a7b3ad
maxwell04 <mahnoorf04@gmail.com>
2024-06-12 19:23:25 +0500

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,13 +12,14 @@ using NamedTrajectories
 
 
 
-include("test_utils.jl")
+# include("test_utils.jl")
 
 @testset "QuantumCollocation.jl" begin
     # include("objectives_test.jl")
     # include("dynamics_test.jl")
     include("integrators_test.jl")
     include("quantum_utils_test.jl")
+    include("quantum_system_templates_test.jl")
 end
 
 @run_package_tests
