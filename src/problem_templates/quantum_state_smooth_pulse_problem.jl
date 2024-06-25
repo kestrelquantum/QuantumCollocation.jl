ed376c138a60361bbd11c39a94e0e67d4389f517
andy <agoldschmidt11@gmail.com>
2024-06-24 21:01:30 -0500

    state smooth pulse piccolo options

--- src/problem_templates/quantum_state_smooth_pulse_problem.jl
+++ src/problem_templates/quantum_state_smooth_pulse_problem.jl
@@ -30,12 +30,12 @@ function QuantumStateSmoothPulseProblem(
     ψ_goal::Union{AbstractVector{<:Number}, Vector{<:AbstractVector{<:Number}}},
     T::Int,
     Δt::Float64;
-    free_time=true,
+    ipopt_options::IpoptOptions=IpoptOptions(),
+    piccolo_options::PiccoloOptions=PiccoloOptions(),
     init_trajectory::Union{NamedTrajectory, Nothing}=nothing,
     a_bound::Float64=1.0,
     a_bounds::Vector{Float64}=fill(a_bound, length(system.G_drives)),
     a_guess::Union{Matrix{Float64}, Nothing}=nothing,
-    rollout_integrator=exp,
     dda_bound::Float64=1.0,
     dda_bounds::Vector{Float64}=fill(dda_bound, length(system.G_drives)),
     Δt_min::Float64=0.5 * Δt,
@@ -47,14 +47,9 @@ function QuantumStateSmoothPulseProblem(
     R_da::Union{Float64, Vector{Float64}}=R,
     R_dda::Union{Float64, Vector{Float64}}=R,
     R_L1::Float64=20.0,
-    max_iter::Int=1000,
-    linear_solver::String="mumps",
-    ipopt_options::IpoptOptions=IpoptOptions(),
     constraints::Vector{<:AbstractConstraint}=AbstractConstraint[],
-    timesteps_all_equal::Bool=true,
     L1_regularized_names=Symbol[],
     L1_regularized_indices::NamedTuple=NamedTuple(),
-    verbose=false,
     kwargs...
 )
     @assert all(name ∈ L1_regularized_names for name in keys(L1_regularized_indices) if !isempty(L1_regularized_indices[name]))
@@ -85,12 +80,12 @@ function QuantumStateSmoothPulseProblem(
             n_drives,
             a_bounds,
             dda_bounds;
-            free_time=free_time,
+            free_time=piccolo_options.free_time,
             Δt_bounds=(Δt_min, Δt_max),
             drive_derivative_σ=drive_derivative_σ,
             a_guess=a_guess,
             system=system,
-            rollout_integrator=rollout_integrator,
+            rollout_integrator=piccolo_options.rollout_integrator,
         )
     end
 
@@ -110,19 +105,19 @@ function QuantumStateSmoothPulseProblem(
                 constraints, name, traj, 
                 R_value=R_L1, 
                 indices=L1_regularized_indices[name],
-                eval_hessian=!hessian_approximation
+                eval_hessian=piccolo_options.eval_hessian
             )
         else
             J += L1Regularizer!(
                 constraints, name, traj; 
                 R_value=R_L1,
-                eval_hessian=!hessian_approximation
+                eval_hessian=piccolo_options.eval_hessian
             )
         end
     end
 
-    if free_time
-        if timesteps_all_equal
+    if piccolo_options.free_time
+        if piccolo_options.timesteps_all_equal
             push!(constraints, TimeStepsAllEqualConstraint(:Δt, traj))
         end
     end
@@ -145,10 +140,8 @@ function QuantumStateSmoothPulseProblem(
         J,
         integrators;
         constraints=constraints,
-        max_iter=max_iter,
-        linear_solver=linear_solver,
-        verbose=verbose,
         ipopt_options=ipopt_options,
+        piccolo_options=piccolo_options,
         kwargs...
     )
 end
@@ -173,6 +166,8 @@ end
     ψ_init = [1.0, 0.0]
     ψ_target = [0.0, 1.0]
 
+    # Single initial and target states
+    # --------------------------------
     prob = QuantumStateSmoothPulseProblem(
         sys, ψ_init, ψ_target, T, Δt;
         ipopt_options=IpoptOptions(print_level=1), verbose=false
@@ -183,6 +178,7 @@ end
     @test final > initial
 
     # Multiple initial and target states
+    # ----------------------------------
     ψ_inits = [[1.0, 0.0], [0.0, 1.0]]
     ψ_targets = [[0.0, 1.0], [1.0, 0.0]]
     prob = QuantumStateSmoothPulseProblem(
