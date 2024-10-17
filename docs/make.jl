2263e5ec396090b09d07f08a32c067f66b224d2b
Aaron Trowbridge <aaron.j.trowbridge@gmail.com>
2024-10-17 15:11:42 -0400

    dependency updates

--- docs/make.jl
+++ docs/make.jl
@@ -14,8 +14,6 @@ pages = [
     "Contribution Guide" => "contribution_guide.md",
     "Manual" => [
         "Problem Templates" => "generated/man/problem_templates.md",
-        "Utilities" => "generated/man/utils.md",
-        "Losses" => "generated/man/losses.md",
         "Embedded Operators" => "generated/man/embedded_operators.md",
     ],
     "Examples" => [
